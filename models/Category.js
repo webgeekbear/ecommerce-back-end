ccb8ce9f32e6aca245e38235bf40f04d505dc0ed
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-12-17 11:27:53 -0800

    Added models

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,32 @@
-const { Model, DataTypes } = require('sequelize');
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
+Category.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [1]
+    }
   }
-);
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'category',
+});
 
 module.exports = Category;
--- models/Product.js
+++ models/Product.js
@@ -7,9 +7,42 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
-    // define columns
+Product.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        decimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        numeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }    
   },
   {
     sequelize,
