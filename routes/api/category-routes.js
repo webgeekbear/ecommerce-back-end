82e07b1306dbf052e951243f28a1036ef812186a
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-12-17 16:23:31 -0800

    Added GET for all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,34 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const {
+  Category,
+  Product
+} = require('../../models');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+      attributes: [
+        'id',
+        'category_name'
+      ],
+      include: [{
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock'
+        ]
+      }]
+    })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
